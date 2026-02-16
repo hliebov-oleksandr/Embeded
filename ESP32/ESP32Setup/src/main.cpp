00017f68da109c689188bcd8a0d388332f5cb6bb
alexander.glebov <glebov.alexander@gmail.com>
2026-02-16 13:39:22 +0200

    radio led display config

--- ESP32/ESP32Setup/src/main.cpp
+++ ESP32/ESP32Setup/src/main.cpp
@@ -1,119 +1,53 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define ENCODER_BUTTON D6
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-#define CONTROL_ANALOG_PHOTO_PIN_1 1
-#define CONTROL_ANALOG_POTENCIOMETR_PIN_2 2
-#define CONTROL_LED_PIN_1 10
-#define CONTROL_LED_PIN_2 11
-#define CONTROL_BUTTON_PIN 9
+#define OLED_DC    D3   // D3 GPIO0
+#define OLED_CS    D8  // D8 GPIO15
+#define OLED_RESET D4   // D4 GPIO2
 
-typedef struct {    
-   int PWWFrequencyGZ;  
-   int* PWWPins;   
-   int PWWPinsLength;   
-   float PWWPeriodMilisec;
-   float PWWDuteTimeMilisec;   
-} PWMConfig;
-int ledPins[] = { CONTROL_LED_PIN_1, CONTROL_LED_PIN_2 };
-int ledPin1[] = { CONTROL_LED_PIN_1 };
-int ledPin2[] = { CONTROL_LED_PIN_2 };
-PWMConfig ledPWWCofig = { .PWWFrequencyGZ = 1000,
-                          .PWWPins = ledPins,
-                          .PWWPinsLength = 2,
-                          .PWWPeriodMilisec = (1.0f / 1000) * 1000.0f }; // (1 sec /  PWWFrequencyGZ) * 1000 ForMilisec                          
-PWMConfig ledPWWCofig1Led = ledPWWCofig;
-PWMConfig ledPWWCofig2Led = ledPWWCofig;
-
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, OLED_DC, OLED_RESET, OLED_CS);
 bool isButtonPressed(int pin);
-void pinPulse(int pin, int pulseDudeTime, int pulseLowTime);
-void pinBlinkInPWWPulse(const PWMConfig& pwwCnfg, int pulseTime);
-void pinLightInPWWPulse(const PWMConfig& pwwCnfg);
-void startBlinking();
-void readAnalogSensors();
-
-//esp 32 s3 series
+void displayInfo(const char* inEndInfo);
 
 void setup() {
-  pinMode(CONTROL_LED_PIN_1 , OUTPUT);
-  pinMode(CONTROL_LED_PIN_2 , OUTPUT);
-  pinMode(CONTROL_BUTTON_PIN , INPUT_PULLDOWN);
-  analogSetAttenuation(ADC_11db);
-  Serial.begin(115200);  
-  ledPWWCofig1Led.PWWDuteTimeMilisec = ledPWWCofig2Led.PWWDuteTimeMilisec = ledPWWCofig.PWWDuteTimeMilisec = ledPWWCofig.PWWPeriodMilisec;                          
-  ledPWWCofig1Led.PWWPins = ledPin1;
-  ledPWWCofig2Led.PWWPins = ledPin2;
-  ledPWWCofig1Led.PWWPinsLength = ledPWWCofig2Led.PWWPinsLength = 1;
+  pinMode(ENCODER_BUTTON, INPUT_PULLUP);
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  displayInfo("");
 }
 
-void loop() {    
-  
-  readAnalogSensors();    
-  if (!isButtonPressed(CONTROL_BUTTON_PIN)) { // Start Blinking
-    pinBlinkInPWWPulse(ledPWWCofig1Led, 1000);
-    pinBlinkInPWWPulse(ledPWWCofig2Led, 1000);    
-  } else { // Lighting both leds       
-    pinLightInPWWPulse(ledPWWCofig);    
+void displayInfo(const char* inEndInfo) {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("FM Radio");
+  display.setTextSize(2);
+  display.println("Station: 101.2 MHz");
+
+  if (sizeof(inEndInfo) / sizeof(inEndInfo[0]) > 0) {
+    display.println(inEndInfo);
   }
+
+  display.display();
 }
 
 bool isButtonPressed(int pin) {
-  return digitalRead(pin) == HIGH ? delay(5), digitalRead(pin) == HIGH : false;
+  return digitalRead(pin) == LOW ? delay(5), digitalRead(pin) == LOW : false;
 }
 
-void startBlinking() {
-  pinPulse(CONTROL_LED_PIN_1, 1000, 0);
-  pinPulse(CONTROL_LED_PIN_2, 1000, 0);  
-  // Stm 32 F1411ccux
-}
-// put function definitions here
+void loop() {
 
-void pinLightInPWWPulse(const PWMConfig& pwwCnfg) {
-  long endTime = millis() + pwwCnfg.PWWPeriodMilisec;  
-  while (millis() <= endTime) {
-      if (pwwCnfg.PWWDuteTimeMilisec > 0) {
-        for (size_t i = 0; i < pwwCnfg.PWWPinsLength ; i++) {     
-          digitalWrite(pwwCnfg.PWWPins[i], HIGH);        
-        }              
-      }      
-      delay(pwwCnfg.PWWDuteTimeMilisec);      
-      for (size_t i = 0; i <  pwwCnfg.PWWPinsLength ; i++) {     
-          digitalWrite(pwwCnfg.PWWPins[i], LOW);              
-      }                    
-      delay(pwwCnfg.PWWPeriodMilisec - pwwCnfg.PWWDuteTimeMilisec);
-  }  
-}
-
-void pinBlinkInPWWPulse(const PWMConfig& pwwCnfg, int pulseTime) {    
-  long endTime = millis() + pulseTime;  
-  while (millis() <= endTime) { 
-    pinLightInPWWPulse(pwwCnfg);
+  if (isButtonPressed(ENCODER_BUTTON)) {
+    Serial.println("Button Pressed");
+    displayInfo("Button is Printed");
   }
-  for (size_t i = 0; i < pwwCnfg.PWWPinsLength; i++) {    
-    digitalWrite(pwwCnfg.PWWPins[i], LOW);
-   }  
 }
-
-void pinPulse(int pin, int pulseDudeTime, int pulseLowTime) {
-  digitalWrite(pin, HIGH);
-  delay(pulseDudeTime);
-  digitalWrite(pin, LOW);
-  delay(pulseLowTime);
-}
-
-void readAnalogSensors() {
-  int analogPhotoResistorVal = analogRead(CONTROL_ANALOG_PHOTO_PIN_1);  
-  int analogPotenciometrPWWVal = analogRead(CONTROL_ANALOG_POTENCIOMETR_PIN_2);  
-  
-  ledPWWCofig1Led.PWWDuteTimeMilisec = ledPWWCofig2Led.PWWDuteTimeMilisec = 
-  ledPWWCofig.PWWDuteTimeMilisec  = (double(analogPotenciometrPWWVal) / 4095.0) * ledPWWCofig.PWWPeriodMilisec;
-
-  Serial.print("Potenciometr val ");
-  Serial.println(analogPotenciometrPWWVal);
-  Serial.print("Milsec Dute Time ");
-  Serial.println(ledPWWCofig.PWWDuteTimeMilisec);
-
-  Serial.print("Photo Resistor ");
-  Serial.println(analogPhotoResistorVal);  
-  Serial.println((analogPhotoResistorVal / 4095.0) * 3.3);  
-}
-
