b4a9d46184244b40cb3ccffe0c68aeb6ce67ac22
hliebov-oleksandr <glebov.alexander@gmail.com>
2026-02-24 16:53:36 +0200

    Module 2.2 Superloop led blinking independet

--- ESP32/ESP32Setup/src/main.cpp
+++ ESP32/ESP32Setup/src/main.cpp
@@ -1,32 +1,31 @@
 #include <Arduino.h>
 
-void buttonLedChangeModeClick();
-
 enum class LedState {
   ON, OFF
 };
 
-class LedWorkGonfig {
+class LedWorkConfig {
   public:
       static constexpr int LED_PIN_1 = 10;
       static constexpr int LED_PIN_2 = 11;
-      static constexpr int BUTTON_CHANHE_MODE_PIN = 9;
-      static constexpr int IS_BUTTON_PRESSED_MILISEC_FLAG = 20;
-      static constexpr int PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC = 1000;
-      static constexpr int PWWPOTENCIOMETR_PIN = 12;      
-      static constexpr int ANALOG_RESOLUTION = 4095;      
+      static constexpr int LED_PIN_3 = 13;     
 };
 
-class PWWLed {  
+class Led {  
   private:
       int ledPin;
       LedState state;      
-
+      long lastStateChange;
   public:
 
-      PWWLed(int pin) : ledPin(pin), state(LedState::OFF) {              
+      Led(int pin) : ledPin(pin), state(LedState::OFF), lastStateChange(0){              
         pinMode(ledPin, OUTPUT);
       }           
+
+      void changeState() {
+         updateState(this->state == LedState::ON ? LedState::OFF : LedState::ON);
+         lastStateChange = millis();
+      }
 
       void updateState(LedState state) {
           this->state = state;
@@ -40,69 +39,35 @@ class PWWLed {
       LedState getState(){
           return this->state;
       }
+
+      long getLastStateChange() {
+        return this->lastStateChange;
+      }
 };
 
-PWWLed led1(LedWorkGonfig::LED_PIN_1);
-PWWLed led2(LedWorkGonfig::LED_PIN_2);
+Led led1(LedWorkConfig::LED_PIN_1);
+Led led2(LedWorkConfig::LED_PIN_2);
+Led led3(LedWorkConfig::LED_PIN_3);
 
-volatile bool isButtonPressed = false;
-
-void setup() {
-  analogSetAttenuation(ADC_11db);  
-  Serial.begin(115200);    
-  pinMode(LedWorkGonfig::BUTTON_CHANHE_MODE_PIN, INPUT_PULLDOWN);
-  attachInterrupt(LedWorkGonfig::BUTTON_CHANHE_MODE_PIN, buttonLedChangeModeClick, CHANGE);  
+void setup() {  
+  Serial.begin(115200);        
 }
 
 void loop() {        
-  static long workTime = 0;
 
-  if (!isButtonPressed) {
-    led1.updateState(LedState::ON);
-    led2.updateState(LedState::ON);
-    workTime = 0;
-  } else {
-      
-      long currentTime = millis();
-      long elapsedTime = currentTime - workTime;
-
-      //mode 1 Blink Light
-      if (elapsedTime <= LedWorkGonfig::PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC) {
-        bool isOn = (currentTime / 50) % 2 == 0;
-        if (isOn) {
-          led1.updateState(LedState::ON);
-          led2.updateState(LedState::ON);
-        } else {
-          led1.updateState(LedState::OFF);
-          led2.updateState(LedState::OFF);
-        }
-      }
-      //mode 2 Turn On
-      else if (elapsedTime > LedWorkGonfig::PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC && 
-        elapsedTime <= LedWorkGonfig::PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC * 2)
-      {
-           led1.updateState(LedState::ON);    
-           led2.updateState(LedState::ON);
-      }
-      //mode 3 Turn off
-      else if (elapsedTime > LedWorkGonfig::PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC * 2 && 
-        elapsedTime <= LedWorkGonfig::PERIOD_TO_CHANGE_LIGHT_MODE_MILISEC * 3)
-      {
-          led1.updateState(LedState::OFF);    
-          led2.updateState(LedState::OFF);
-      }
-      else {
-        workTime = currentTime;
-      }
-  }
-   
-}
-
-void IRAM_ATTR buttonLedChangeModeClick() {   
     long currentTime = millis();
-    if (digitalRead(LedWorkGonfig::BUTTON_CHANHE_MODE_PIN) == HIGH) {
-       isButtonPressed = true;       
-    } else if (digitalRead(LedWorkGonfig::BUTTON_CHANHE_MODE_PIN) == LOW){
-      isButtonPressed = false;     
-    }    
+    
+    if (currentTime - led1.getLastStateChange() > 200) {
+       led1.changeState();
+    }
+
+    if (currentTime - led2.getLastStateChange() > 500) {
+       led2.changeState();
+    }
+
+    if (currentTime - led3.getLastStateChange() > 1000) {
+       led3.changeState();
+    }
+    
 }
+
