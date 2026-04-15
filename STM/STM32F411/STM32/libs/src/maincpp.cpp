2c557bdf9b979b5309cc640891d173aa6252dcba
hliebov-oleksandr <glebov.alexander@gmail.com>
2026-04-15 21:57:05 +0300

    Write data to screen SSD1306 i2c

--- STM/STM32F411/STM32/libs/src/maincpp.cpp
+++ STM/STM32F411/STM32/libs/src/maincpp.cpp
@@ -6,13 +6,14 @@
 #include "bme280.h"
 #include "stdio.h"
 
-//extern "C" s32 bme280_init();
+// extern "C" s32 bme280_init();
 extern "C" s32 bme280_data_readout_template(void);
 extern "C" s32 bme280_data_readout_template(void);
 extern "C" BME280_RETURN_FUNCTION_TYPE bme280_read_pressure_temperature_humidity(
-u32 *v_pressure_u32, s32 *v_temperature_s32, u32 *v_humidity_u32);
+    u32 *v_pressure_u32, s32 *v_temperature_s32, u32 *v_humidity_u32);
 
-typedef struct {
+typedef struct
+{
     s32 temp;
     u32 press;
     u32 hum;
@@ -34,7 +35,6 @@ uint8_t logData[200];
 uint16_t logDataLen;
 */
 
-
 STMGPIO gpioBlueLed(GPIOA, GPIO_PIN_5);
 char data[] = "HELLO World AAAA AA\n";
 
@@ -42,49 +42,77 @@ void setup()
 {
     SSD1306_Init();
     SSD1306_GotoXY(0, 0);
-    
+
     SSD1306_GotoXY(0, 30);
     SSD1306_Puts("TIME 12:23:00", &Font_11x18, SSD1306_COLOR_t::SSD1306_COLOR_WHITE);
     SSD1306_UpdateScreen();
-    HAL_Delay(1000);
-    SSD1306_ScrollRight(0, 17);
-    HAL_Delay(3000);
-    SSD1306_ScrollLeft(0, 17);
-    HAL_Delay(3000);
-    SSD1306_Stopscroll();
+}
+
+void writeData(char *data)
+{
     
-    gpioBlueLed.writeGPIO(true);
+    int len = strlen(data);
+    HAL_UART_Transmit(&huart2, (uint8_t *)data, len, HAL_MAX_DELAY);
+    char *delimetr = "\n";
+    const char *start = data;
+    const char *found_str;
+    int position, lasrposition;
+    char splitedData[3][64];
+    int countRows = 0;
+
+    while ((found_str = strstr(start, delimetr)) != NULL)
+    {
+        position = found_str - start;        
+        memcpy(splitedData[countRows], start, sizeof(char) * position);
+        splitedData[countRows][position] = '\0';
+        HAL_UART_Transmit(&huart2, (uint8_t *)splitedData[countRows], position, HAL_MAX_DELAY);
+        start = found_str + strlen(delimetr);
+        countRows++;
+    }
+
+    SSD1306_Clear();
+    int j = 10;
+    for (int i = 0; i < countRows; i++)
+    {
+        //
+        SSD1306_GotoXY(0, j);
+        SSD1306_Puts(splitedData[i], &Font_11x18, SSD1306_COLOR_t::SSD1306_COLOR_WHITE);
+        SSD1306_UpdateScreen();
+        j += 16;        
+        //
+    }
+    
+    HAL_UART_Transmit(&huart2, (uint8_t *)data, len, HAL_MAX_DELAY);
 }
 
 void loop(void *params)
-{    
+{
     const char msg[] = "Hello World\r\n";
-    HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-    
+    HAL_UART_Transmit(&huart2, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+
     gpioBlueLed.writeGPIO(!gpioBlueLed.readGPIO());
     HAL_Delay(2000);
 
-    
     bme280_values_t termo;
     s32 status = bme280_data_readout_template();
-    
+
     char buf[32];
     int len = snprintf(buf, sizeof(buf), "status=%ld\r\n", (long)status);
-    HAL_UART_Transmit(&huart2, (uint8_t*)buf, len, HAL_MAX_DELAY);             
-    if (status == 0) {
-        bme280_read_pressure_temperature_humidity(&termo.press, &termo.temp, &termo.hum);        
+    writeData(buf);
+    if (status == 0)
+    {
+        bme280_read_pressure_temperature_humidity(&termo.press, &termo.temp, &termo.hum);
         char buf[64];
         int len = snprintf(buf, sizeof(buf),
-                           "T=%ld C, P=%lu Pa, H=%lu %%\r\n",
-                           (long)termo.temp/100,
-                           (unsigned long)termo.press/100,
-                           (unsigned long)termo.hum/1024);
-        HAL_UART_Transmit(&huart2, (uint8_t*)buf, len, HAL_MAX_DELAY);
-    } else {
-        const char err[] = "BME280 read error\r\n";
-        HAL_UART_Transmit(&huart2, (uint8_t*)err, strlen(err), HAL_MAX_DELAY);
+                           "T=%ld C \nP=%lu Pa \nH=%lu %% \n",
+                           (long)termo.temp / 100,
+                           (unsigned long)termo.press / 100,
+                           (unsigned long)termo.hum / 1024);
+        writeData(buf);
     }
-    
-    SSD1306_ScrollRight(0, 17);    
-    SSD1306_ScrollLeft(0, 17);
+    else
+    {
+        char err[] = "bme280 \nread \nerror\n";
+        writeData(err);
+    }
 }
